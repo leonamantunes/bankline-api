553241383098d06fd737ec18fc9fbd54bccb57fd
Leonam Antunes <leonamc.antunes@gmail.com>
2022-05-05 16:09:45 -0300

    criei as classes de movimentacao

--- src/main/java/com/dio/santander/bankline/api/BanklineApiApplication.java
+++ src/main/java/com/dio/santander/bankline/api/BanklineApiApplication.java
@@ -1,4 +1,4 @@
-package com.dio.santander.banklineapi;
+package com.dio.santander.bankline.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,7 +9,6 @@ public class BanklineApiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BanklineApiApplication.class, args);
 		
-		//iniciando o codigo do projeto	
 	}
 
 }
